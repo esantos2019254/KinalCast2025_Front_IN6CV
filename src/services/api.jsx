e89b67274775a8dac9750e550b800452319c4d5e
esantos2019254 <esantos-2019254@kinal.edu.gt>
2025-04-09 17:16:07 -0600

    Lista de canales, sidebar terminado

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,9 +1,26 @@
 import axios from "axios";
+import { logout } from "../shared/hooks"
 
 const apiClient = axios.create({
     baseURL: "http://127.0.0.1:8080/twitch/v1",
     timeout: 5000
 })
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const useUserDetails = localStorage.getItem('user');
+
+        if(useUserDetails){
+            const token = JSON.parse(useUserDetails).token
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config;
+    },
+    (e) => {
+        return Promise.reject(e);
+    }
+)
 
 export const login = async (data) => {
     try {
@@ -24,5 +41,36 @@ export const register = async (data) => {
             error: true,
             e
         }
+    }
+}
+
+export const getChannels = async () => {
+    try {
+        return await apiClient.get('/channels')
+    } catch (error) {
+        return {
+            error: true,
+            e
+        }
+    }
+}
+
+export const getFollowedChannels = async () => {
+    try {
+        return await apiClient.get('/channels/followed')
+    } catch (e) {
+        checkResponseStatus(e)
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
+const checkResponseStatus = (e) => {
+    const responseStatus = e?.response?.status
+
+    if(responseStatus){
+        (responseStatus === 401 || responseStatus === 403) && logout()
     }
 }
